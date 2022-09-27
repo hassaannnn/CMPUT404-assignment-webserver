b69f03630c20f210e57588cda078a556e8b8f8d1
has <hassaan3@ualberta.ca>
2022-09-26 21:50:25 -0600

    adding most functionality and pics

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #  coding: utf-8 
-import socketserver
+import socketserver,os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -31,8 +31,98 @@ class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        #print ("Got a request of: %s\n" % self.data.decode())
+        allReqDetails = self.data.decode().split()
+        reqType = allReqDetails[0]
+        reqPath = allReqDetails[1]
+        if(reqType != "GET"):
+            self.handleIllegal()
+            return
+        else:
+            self.handleRegularResponse(reqPath)
+             
+            #self.handleIllegal()
+        #print(allReqDetails)
+        #self.request.sendall(bytearray("OK",'utf-8'))
+    def handleIllegal(self):
+        self.request.sendall(bytearray("HTTP/1.1 405 Method Not Allowed\r\n\r\n405 Method Not Allowed",'utf-8'))
+        return
+    def handleIllegalPath(self):
+        self.request.sendall(bytearray("HTTP/1.1 404 Not Found\r\n\r\n404 Not found",'utf-8'))
+        return
+    def handleSuccess(self, content, mimeType):
+        successResponse = "HTTP/1.1 200 OK\r\nContent-Type: text/" + mimeType+"\r\n\r\n" + content
+        #print(successResponse)
+        self.request.sendall(bytearray(successResponse,'utf-8'))
+        return
+    def handleRedirect(self, path):
+        print("redirecting")
+        newUrl = path + "/"+ "index.html"
+        rawNewUrl = (path + "/").replace("./www","")
+        response = "HTTP/1.1 301 Move Permanently\r\nLocation: " + rawNewUrl + "\r\nContent-Type: text/html\r\n\r\n"
+
+        try:
+            resFile=open(newUrl, "r")
+            fileCont = resFile.read()
+            response += fileCont
+            self.request.sendall(bytearray(response,'utf-8'))
+            return
+        except Exception as e:
+            print("File is not found " , e )
+            self.handleIllegalPath()
+            return
+    def handleNormal(self,path):
+        newUrl = path + "index.html"
+        try:
+            resFile = open(newUrl, "r")
+            resCont = resFile.read()
+            self.handleSuccess(resCont, "html")
+            return
+        except:
+            self.handleIllegalPath()
+            return
+            
+        
+    def handleRegularResponse(self,reqPath):
+        
+        if("../" in reqPath):# Security check
+            print("illegal path")
+            self.handleIllegalPath()
+            return
+        
+        fileRoot = './www'
+        path = fileRoot + reqPath
+        #print(reqPath)
+        #print(path)
+        if(len(reqPath.split(".")) == 2):# If there is file extension(mime specified)
+            mimeType = reqPath.split(".")[1]
+            #resFile=open(path, "r")
+            
+            try: # check if they want specific site
+                resFile=open(path, "r")
+                fileCont = resFile.read()
+                self.handleSuccess( fileCont,mimeType)
+                return
+            except Exception as e:
+                print("File is not found " , e )
+                self.handleIllegalPath()
+                return
+        else:# Check directory serve index.html
+            # handle redirection
+            print("serving index")
+            if(path[-1] != "/"):
+                self.handleRedirect(path)
+                return
+            else:
+                print("serving index regularyly")
+                self.handleNormal(path)
+                return
+            
+
+
+        
+        
+        
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
